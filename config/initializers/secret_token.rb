cd71416ed75d63bb38b5a776d829f802a1182bf2
Bryan Bibat <bry@bryanbibat.net>
2013-01-07 01:14:23 +0800

    Change token for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pinger::Application.config.secret_token = '1afdd1d54c7b9e9937e111911ddd5d9fe72d94f50a7d01dff8ccd2f3fb7e834ec188f30665ffd0a03707058f0a92ddc1bc735e8b42b36c933d5ff5b78c217c93'
+Pinger::Application.config.secret_token = '57b13160861b3dd0c3c065427e8edeca49afea8daf647bab88faff0023de9927379f385fde5a7443c11ae6c8b88105d599b15c56775df8feb9c27a6dbe48e5f9'
